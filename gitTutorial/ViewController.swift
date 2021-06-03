e88a9f5bbe658562df44f8dc952d35dd7a977921
lidongyang <lidongyang@yuanfudao.com>
2021-06-03 10:15:29 +0800

    a 3rd commit

--- gitTutorial/ViewController.swift
+++ gitTutorial/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("hello World~~")
         print("print another line")
+        print("a another line")
     }
 
     func didSomeChange() {
